2bb28d447682191ef840025c3a1326611ec30131
iMac <imac@iMacs-iMac-2.local>
2020-05-24 23:29:15 +0700

    fixed bug, chagne par accumulation interval

--- src/main.cpp
+++ src/main.cpp
@@ -217,7 +217,7 @@ Task tGetTemperature(2000L, TASK_FOREVER, &fGetTemperature, &schMain, true);
 Task tGetHumidity(2050L, TASK_FOREVER, &fGetHumidity, &schMain, true);
 Task tGetSoilTemperature(2200L, TASK_FOREVER, &fGetSoilTemperature, &schMain, true);
 Task tGetSoil(2100L, TASK_FOREVER, &fGetSoil, &schMain, true);
-Task tGetPar(2150L, TASK_FOREVER, &fGetPar, &schMain, true);
+Task tGetPar(1000L, TASK_FOREVER, &fGetPar, &schMain, true);
 Task tGetCO2(2200L, TASK_FOREVER, &fGetCO2, &schMain, true);
 Task tCheckRequestAndResponse(50, TASK_FOREVER, &fCheckRequestAndResponse, &schCom, false);
 
